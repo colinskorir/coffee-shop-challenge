e642a28a97805778d26463013648885d460b6566
colinskorir <korircoll20@gmail.com>
2025-05-19 00:41:10 +0300

    new coffee added

---
+++ coffee-shop-challenge/coffee_shop_challenge/coffee.py
@@ -0,0 +1,19 @@
+class Coffee:
+    def __init__(self, name):
+        self._name = None
+        self.name = name
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if len(value) < 3:
+            raise ValueError("Name must be at least 3 characters")
+        if self._name is not None:
+            raise AttributeError("Coffee name cannot be changed after initialization")
+        self._name = value
