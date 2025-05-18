9f80d34289c2570f2a3c69277d58ac4f1f9ab4b0
colinskorir <korircoll20@gmail.com>
2025-05-19 00:48:09 +0300

    modified order

--- coffee-shop-challenge/coffee_shop_challenge/order.py
+++ coffee-shop-challenge/coffee_shop_challenge/order.py
@@ -18,3 +18,15 @@ class Order:
         
         customer.orders().append(self)
         coffee.orders().append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property
+    def coffee(self):
+        return self._coffee
