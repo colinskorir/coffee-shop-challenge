fe304b15aefeb65a148f0d4c4a56eae1239d138d
colinskorir <korircoll20@gmail.com>
2025-05-19 00:47:30 +0300

    new order

---
+++ coffee-shop-challenge/coffee_shop_challenge/order.py
@@ -0,0 +1,14 @@
+from coffee_shop_challenge.customer import Customer
+from .coffee import Coffee
+
+class Order:
+    def __init__(self, customer, coffee, price):
+        if not isinstance(customer, Customer):
+            raise TypeError("Customer must be a Customer instance")
+        if not isinstance(coffee, Coffee):
+            raise TypeError("Coffee must be a Coffee instance")
+        if not isinstance(price, float):
+            raise TypeError("Price must be a float")
+        if not 1.0 <= price <= 10.0:
+            raise ValueError("Price must be between 1.0 and 10.0")
+        
