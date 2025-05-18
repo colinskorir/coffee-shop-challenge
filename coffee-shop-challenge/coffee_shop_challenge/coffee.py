200ea439886a94a94276fa7f1f12bdc7a40eca95
colinskorir <korircoll20@gmail.com>
2025-05-19 00:41:54 +0300

    modified coffee added

--- coffee-shop-challenge/coffee_shop_challenge/coffee.py
+++ coffee-shop-challenge/coffee_shop_challenge/coffee.py
@@ -23,3 +23,11 @@ class Coffee:
 
     def customers(self):
         return list(set(order.customer for order in self._orders))
+
+    def num_orders(self):
+        return len(self._orders)
+
+    def average_price(self):
+        if not self._orders:
+            return 0
+        return sum(order.price for order in self._orders) / len(self._orders) 
